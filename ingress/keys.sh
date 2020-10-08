3a54771adfc6badc3ecef8bfcd01d081b58a58a7
Gav <gavin.pollitt@btinternet.com>
2020-10-08 08:47:19 +0100

    Added in the oauth2 capability

--- ingress/keys.sh
+++ ingress/keys.sh
@@ -2,5 +2,5 @@ export KEY_FILE=./queue.key
 export CERT_FILE=./queue.cert
 export HOST=gav.wsl2
 export CERT_NAME=gavq
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
 kubectl create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE} -n client
