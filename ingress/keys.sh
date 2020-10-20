803a6b8aa4e2f6c83ffcf43ff7aa46da69e2779a
Gavin Pollitt <gavin.pollitt@btinternet.com>
2020-10-20 09:55:20 +0100

    Made ARM compliant

--- ingress/keys.sh
+++ ingress/keys.sh
@@ -1,6 +1,6 @@
 export KEY_FILE=./queue.key
 export CERT_FILE=./queue.cert
-export HOST=gav.wsl2
+export HOST=gav-wsl2.poll.com
 export CERT_NAME=gavq
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
 kubectl create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE} -n client
